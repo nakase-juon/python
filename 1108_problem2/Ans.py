065be7715359d4c1561e2f55097bdf0ad976d1ba
nakase-juon <63778915+nakase-juon@users.noreply.github.com>
2022-11-08 23:16:43 +0900

    merge program

---
+++ 1108_problem2/Ans.py
@@ -0,0 +1,60 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from statistics import stdev
+
+pi = int()
+n = int()
+
+
+def monte_carlo():
+    global n
+    global pi
+    
+    n_in = 0  # 円の中に入った乱数の個数の初期値
+    x1 = np.random.rand(n, 2)
+
+    # モンテカルロ法
+    for i in range(n):
+        if (x1[i, 0]**2+x1[i, 1]**2)**0.5 < 1:  # 半径1の円の内側に入る場合
+            n_in = n_in+1
+    pi = 4*(n_in/n)
+
+def meanAndStdev():
+    global n
+    global pi
+    
+    pi_list = list()
+    mean_list = list()
+    stdev_list = list()
+
+    n = 100
+    plt.figure(figsize=(10, 5))
+    while n <= 10000:
+        monte_carlo()
+        pi_list.append(pi)
+        if(len(pi_list) > 1):
+            mean_list.append(sum(pi_list)/len(pi_list))
+            stdev_list.append(stdev(pi_list))
+        n += 200
+
+    x = [x for x in range(200, 10000, 200)]
+
+    plt.subplot(121)
+    plt.xlabel('number of points')
+    plt.ylabel('average of obtained π')
+    plt.xlim(-500, 10500)
+    plt.grid()
+    plt.plot(x, mean_list, color='red')
+
+    plt.subplot(122)
+    plt.xlabel('number of points')
+    plt.ylabel('standard deviation of obtained π')
+    plt.xlim(-500, 10500)
+    plt.grid()
+    plt.plot(x, stdev_list, color='red')
+
+    plt.tight_layout()
+    plt.show()
+
+if __name__ == '__main__':
+    meanAndStdev()
