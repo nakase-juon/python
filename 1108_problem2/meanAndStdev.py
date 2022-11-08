e15b1bc6ba2f2e5a433b1e4bdfcbef356b8ffe7a
nakase-juon <63778915+nakase-juon@users.noreply.github.com>
2022-11-08 21:13:21 +0900

    Fixed problem2

--- 1108_problem2/meanAndStdev.py
+++ 1108_problem2/meanAndStdev.py
@@ -4,7 +4,7 @@ from statistics import stdev
 
 import Pi as p
 
-pi_list = list()# N回の試行結果を格納するリスト
+pi_list = list()
 mean_list = list()
 stdev_list = list()
 
@@ -13,26 +13,26 @@ plt.figure(figsize=(10, 5))
 while p.n <= 10000:
     p.monte_carlo()
     pi_list.append(p.pi)
-    mean_list.append(sum(pi_list)/len(pi_list))
     if(len(pi_list) > 1):
+        mean_list.append(sum(pi_list)/len(pi_list))
         stdev_list.append(stdev(pi_list))
-    p.n += 1
+    p.n += 200
+
+x = [x for x in range(200, 10000, 200)]
 
 plt.subplot(121)
 plt.xlabel('number of points')
 plt.ylabel('average of obtained π')
-plt.xlim(0, 10000)
-plt.ylim(3.1, 3.17)
+plt.xlim(-500, 10500)
 plt.grid()
-plt.plot(mean_list, color='red')
+plt.plot(x, mean_list, color='red')
 
 plt.subplot(122)
 plt.xlabel('number of points')
 plt.ylabel('standard deviation of obtained π')
-plt.xlim(0, 10000)
-plt.ylim(0, 0.15)
+plt.xlim(-500, 10500)
 plt.grid()
-plt.plot(stdev_list, color='red')
+plt.plot(x, stdev_list, color='red')
 
 plt.tight_layout()
 plt.show()
